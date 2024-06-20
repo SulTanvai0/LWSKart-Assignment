e8c9b5d21780efb5c5e24469116a994db6a6f981
Muhammed Mehedi hasan <108600250+SulTanvai0@users.noreply.github.com>
2024-06-20 16:24:14 -0700

    add arrival

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -20,8 +20,8 @@ export default function Homepage({ params: { lang } }) {
       <Features lang={lang} />
       {console.log("Features section loaded")}
       <Category lang={lang} />
-      {/* {console.log("Arrival section loaded")}
-      <Arrival lang={lang} /> */}
+       {console.log("Arrival section loaded")}
+      <Arrival lang={lang} /> 
       <Ads />
      {/*  <Trending lang={lang} />
       {console.log("Trending section loaded")} */}
