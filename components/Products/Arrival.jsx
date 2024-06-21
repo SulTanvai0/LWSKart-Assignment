656f1785058d5bd16e63461f3d1e213f228264e9
Muhammed Mehedi hasan <108600250+SulTanvai0@users.noreply.github.com>
2024-06-20 17:42:13 -0700

    arrival

--- components/Products/Arrival.jsx
+++ components/Products/Arrival.jsx
@@ -6,7 +6,7 @@ const Arrival = async ({ lang }) => {
 
     const Products = await getArrivalData()
     console.log("Fetched Products:", Products);
-    
+
     const { newArrival, noFound } = await getDictionary(lang);
 
     return (
@@ -14,9 +14,9 @@ const Arrival = async ({ lang }) => {
             <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">{newArrival}</h2>
 
             {
-                Products?.data.length > 0 ? <> <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 justify-items-center">
+                Products?.length > 0 ? <> <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 justify-items-center">
 
-                    {Products?.data?.map((product) => (
+                    {Products?.map((product) => (
 
                         <Card key={product?._id} product={product} />
 
